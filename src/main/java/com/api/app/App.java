e64aacb860011491978517828080955639e1921e
Theo <theokimbangui36@gmail.com>
2025-01-06 21:26:39 +0100

    feat: create local server and first endpoint

--- src/main/java/com/api/app/App.java
+++ src/main/java/com/api/app/App.java
@@ -1,10 +1,32 @@
 package com.api.app;
 
-/**
- * Hello world!
- */
+import com.sun.net.httpserver.HttpServer;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpExchange;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
 public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        server.createContext("/api/hello", new HelloHandler());
+
+        server.setExecutor(null);
+        server.start();
+        System.out.println("Server started on http://localhost:8000");
+    }
+}
+
+class HelloHandler implements HttpHandler {
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String response = "{\"message\":\"Hello, World!\"}";
+        exchange.getResponseHeaders().set("Content-Type", "application/json");
+        exchange.sendResponseHeaders(200, response.getBytes().length);
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
     }
 }
